314db0ee46644cd464fe61e1fa9b2049662d1887
Apurv Chatur <official.apurv.chatur@gmail.com>
2026-09-17 19:45:09 +0530

    TIC-012: env bug (backend)

--- site/backend/src/aConnection/EnvironmentConnection.ts
+++ site/backend/src/aConnection/EnvironmentConnection.ts
@@ -13,19 +13,19 @@ class EnvironmentConnection {
 
     this.ENV = this.defaultConfig.parsed?.NODE_ENV as string;
     this.MACHINE = this.defaultConfig.parsed?.NODE_MACHINE as string;
-    this.PORT = Number(this.defaultConfig.parsed?.PORT || process.env.PORT || 7999) as number;
+    this.PORT = Number(this.defaultConfig.parsed?.PORT) as number;
     this.APP = this.defaultConfig.parsed?.NODE_APP as string;
 
     if (this.MACHINE === "local") {
-      const path = this.connectFolder();
-      
-      this.ENV = process.env.NODE_ENV as string;
-      this.MACHINE = process.env.NODE_MACHINE as string;
-      this.PORT = Number(process.env.PORT) as number;
-      this.APP = process.env.NODE_APP as string;
+      const path = this.connectFolder();      
 
       console.log(`Environment connection created successfully at path: "${path}"`);
     }
+    
+    this.ENV = process.env.NODE_ENV as string;
+    this.MACHINE = process.env.NODE_MACHINE as string;
+    this.PORT = Number(process.env.PORT) as number;
+    this.APP = process.env.NODE_APP as string;
   }
 
   private connectFolder() {
